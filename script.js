aa82912eb08c297d72caecb2473ef9d8e53fae4f
dev-andersonmelo <89094008+dev-andersonmelo@users.noreply.github.com>
2022-05-11 08:26:28 -0300

    Add files via upload
    Primeiro commit.

---
+++ script.js
@@ -0,0 +1,44 @@
+// Scripts por Anderson Melo
+
+var myButtonModal = document.getElementById('myButtonModal')
+var myTextModal = document.getElementById('myTextModal')
+
+var inputUsuario = document.getElementById('inputUsuario')
+var inputSenha = document.getElementById('inputSenha')
+
+function autenticar(event){
+  // escopo função autenticar
+
+  event.preventDefault()
+
+    if(inputUsuario.value == 'admin' && inputSenha.value == '1234'){
+      myTextModal.className = 'text-success'
+      myTextModal.innerText = 'Usuário Logado!!!'
+  } else {
+      myTextModal.className = 'text-danger'
+      myTextModal.innerText = 'Usuário ou senha inválidos!!!'
+  }
+  
+  myButtonModal.click()
+}
+
+// var myButtonModal = document.getElementById('myButtonModal')
+// var myTextModal = document.getElementById('myTextModal')
+
+// var inputUsuario = document.getElementById('inputUsuario')
+// var inputSenha = document.getElementById('inputSenha')
+
+// function autenticar(event){
+//   // escopo função autenticar
+//   event.preventDefault()
+
+//   if(inputUsuario.value == 'admin' && inputSenha.value == '1234'){
+//     myTextModal.className = 'text-success'
+//     myTextModal.innerText = 'Usuário Logado!!!'
+//   } else {
+//     myTextModal.className = 'text-danger'
+//     myTextModal.innerText = 'Usuário ou senha inválidos!!!'
+//   }
+
+//   myButtonModal.click()
+// }
